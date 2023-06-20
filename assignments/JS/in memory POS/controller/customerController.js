defa7be0b5164864e7c4df3e8295fefb969402f0
achinthajayashan <achintha.uns.2018@gmail.com>
2023-06-20 18:08:22 +0530

    customer bind Events

--- assignments/JS/in memory POS/controller/customerController.js
+++ assignments/JS/in memory POS/controller/customerController.js
@@ -1,25 +1,72 @@
 $('#btnSaveCustomer').click(function () {
-    saveCustomer();
-    console.log(customerdb);
+    if (checkAll()){
+         saveCustomer();
+        // console.log(customerdb);
+    }else {
+        alert("Error When Saving...")
+    }
+    clearCustomerInputFields();
     clearCusTextFields();
+
+    //getAllCustomers();
+})
+
+$('#btnUpdateCus').click(function () {
+    let id = $("#InputCID").val();
+    updateCustomer(id);
+    clearCusTextFields();
+    clearCustomerInputFields();
+   // getAllCustomers();
+})
+
+$('#btnGetAllCus').click(function () {
     getAllCustomers();
 })
 
+$("#btnDeleteCustomer").click(function () {
+    let id = $("#InputCID").val();
+
+    let consent = confirm("Do you want to delete.?");
+    if (consent) {
+        let response = deleteCustomer(id);
+        if (response) {
+            clearCusTextFields();
+            clearCustomerInputFields();
+            getAllCustomers();
+            alert("Customer Deleted");
+        } else {
+            alert("Customer Not Removed..!");
+        }
+    }
+
+
+});
+
 function saveCustomer() {
     let customerID = $("#InputCID").val();
-    let customerName = $("#InputName").val();
-    let customerAddress = $("#inputAddress").val();
-    let customerContact = $("#inputContact").val();
+    if (searchCustomer(customerID.trim()) == undefined) {
 
-    let newCustomer = Object.assign({}, customer);
-    newCustomer.id = customerID;
-    newCustomer.name = customerName;
-    newCustomer.address = customerAddress;
-    newCustomer.contact = customerContact;
+        let customerName = $("#InputName").val();
+        let customerAddress = $("#inputAddress").val();
+        let customerContact = $("#inputContact").val();
 
-    //add customer record to the customer array
-    customerdb.push(newCustomer);
+        let newCustomer = Object.assign({}, customer);
+        newCustomer.id = customerID;
+        newCustomer.name = customerName;
+        newCustomer.address = customerAddress;
+        newCustomer.contact = customerContact;
 
+        //add customer record to the customer array
+        customerdb.push(newCustomer);
+        getAllCustomers();
+        clearCustomerInputFields();
+        clearCusTextFields();
+    }
+ else {
+    alert("Customer already exits.!");
+    clearCustomerInputFields();
+    clearCusTextFields();
+}
 }
 
 
@@ -42,9 +89,6 @@ function getAllCustomers() {
 
         $("#tableBodyCus").append(row);
 
-        //invoke this method every time
-        // we add a row // otherwise click
-        //event will not work
         bindTrEvents();
     }
 }
@@ -63,6 +107,48 @@ function bindTrEvents() {
         $("#inputAddress").val(address);
         $("#inputContact").val(contact);
     })
+}
+
+
+function searchCustomer(id) {
+    return customerdb.find(function (customer) {
+        //if the search id match with customer record
+        //then return that object
+        return customer.id == id;
+    });
+}
+
+function updateCustomer(id) {
+    if (searchCustomer(id) == undefined) {
+        alert("No such Customer..please check the ID");
+    } else {
+        let consent = confirm("Do you really want to update this customer.?");
+        if (consent) {
+            let customer = searchCustomer(id);
+            //if the customer available can we update.?
+
+            let customerName = $("#InputName").val();
+            let customerAddress = $("#inputAddress").val();
+            let customerContact = $("#inputContact").val();
+
+            customer.name = customerName;
+            customer.address = customerAddress;
+            customer.contact = customerContact;
+
+            getAllCustomers();
+        }
+    }
+
+}
+
+function deleteCustomer(id) {
+    for (let i = 0; i < customerdb.length; i++) {
+        if (customerdb[i].id == id) {
+            customerdb.splice(i, 1);
+            return true;
+        }
+    }
+    return false;
 }
 
 
